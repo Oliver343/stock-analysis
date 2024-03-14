018e763b93059e6f9b39dcaee36964fb2d01b81e
Oliver Sanderson <oliver_343@hotmail.com>
2024-03-14 12:25:56 +0100

    added basic fetch

---
+++ src/components/Fetch.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+export default function Fetch() {
+    const [displayQuote, setDisplayQuote] = useState('Loading')
+    const key = "" // Current URL doesnt require key
+
+    useEffect(() => {
+        fetchQuote()
+    }, [])
+
+    async function fetchQuote() {
+        const response = await fetch("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=" + key);
+        const quote = await response.json();
+        setDisplayQuote(quote["Global Quote"]["01. symbol"])
+        console.log(quote);
+      }
+
+    return(
+        <div>
+            <h1>Fetched Quote:</h1>
+            <p>{displayQuote}</p>
+        </div>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Fetch from "../components/Fetch";
 import Header from "../components/Header";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     return(
     <div className="home">
         <Header />
+        <Fetch />
         HOME PAGE CONTENT
     </div>
     )
