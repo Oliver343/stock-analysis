4632843df2c6f416ddba58ea881ca4b3829a6681
Oliver Sanderson <oliver_343@hotmail.com>
2024-03-15 12:31:07 +0100

    fetch error handled

--- src/components/Fetch.jsx
+++ src/components/Fetch.jsx
@@ -9,10 +9,23 @@ export default function Fetch() {
     }, [])
 
     async function fetchQuote() {
-        const response = await fetch("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=" + key);
-        const quote = await response.json();
-        setDisplayQuote(quote["Global Quote"]["01. symbol"])
-        console.log(quote);
+        try {
+            const response = await fetch("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=" + key);
+            console.log(response)
+            const quote = await response.json();
+            if(response.ok) {
+                setDisplayQuote(quote["Global Quote"]["01. symbol"])
+                console.log(quote);
+            } else {
+                setDisplayQuote("There was a problem loading the data.")
+            }
+          } catch (error) {
+            // TypeError: Failed to fetch
+            console.log('There was an error', error);
+            setDisplayQuote("Failed to fetch information. Please try again later.")
+          }
+
+
       }
 
     return(
